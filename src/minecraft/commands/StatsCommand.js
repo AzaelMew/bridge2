f5ada87f82b7de5b22a50242ec9bf7de1e03ca7a
AzaelMew <dogydemon@gmail.com>
2023-01-12 14:57:20 +0200

    a

--- src/minecraft/commands/StatsCommand.js
+++ src/minecraft/commands/StatsCommand.js
@@ -57,9 +57,8 @@ async function getStatsFromUUID(name,profile) {
         let eslayer = data.data[i]?.slayer?.enderman.xp
         let bslayer = data.data[i]?.slayer?.blaze.xp
         let sblvl = data.data[i]?.sblevel
-        sblvl = sblvl.split(".")
         let slayer = numberWithCommas(wslayer + zslayer + sslayer + eslayer + bslayer)
-        let stats = `On ${profile}: \nSkyblock Level: ${sblvl[0]}; Skill Avg: ${sa}; Slayer: ${slayer}; Cata: ${cata}; Networth: $${nw}`
+        let stats = `On ${profile}: \nSkyblock Level: ${sblvl.toFixed(0)}; Skill Avg: ${sa}; Slayer: ${slayer}; Cata: ${cata}; Networth: $${nw}`
         return stats
       }
       else if (i == Object.keys(data.data).length - 1){
