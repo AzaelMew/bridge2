c22789a726e0f4e5807708d06e45148054dc503d
AzaelMew <dogydemon@gmail.com>
2023-01-12 12:50:13 +0200

    a

--- src/discord/commands/StatsCommand.js
+++ src/discord/commands/StatsCommand.js
@@ -73,7 +73,7 @@ async function getStatsFromUUID(name,profile) {
         let sslayer = data.data[0]?.slayer?.spider.xp
         let eslayer = data.data[0]?.slayer?.enderman.xp
         let bslayer = data.data[0]?.slayer?.blaze.xp
-        let sblvl = data.data[i]?.sblevel
+        let sblvl = data.data[0]?.sblevel
         let slayer = numberWithCommas(wslayer + zslayer + sslayer + eslayer + bslayer)
         let stats = `Skill Avg: ${sa}; Skyblock Level: ${sblvl}; Slayer: ${slayer}; Cata: ${cata}; Networth: $${nw}; `
         return stats
