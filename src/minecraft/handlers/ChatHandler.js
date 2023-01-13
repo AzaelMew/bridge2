65b9cdf6d42e252442bb774b377b97a8860a7b14
AzaelMew <dogydemon@gmail.com>
2023-01-13 09:37:54 +0200

    a

--- src/minecraft/handlers/ChatHandler.js
+++ src/minecraft/handlers/ChatHandler.js
@@ -261,7 +261,7 @@ class StateHandler extends EventHandler {
       mes = reta
       reta = ""
       mes = mes.replaceAll("_", "\\_")
-      return this.minecraft.broadcastCleanEmbed({ message: mes, color: '47F049' })
+      return this.minecraft.broadcastCleanEmbed({ message: mes, color: '2A2A2A' })
     }
 
     if (this.isGTopMessage(message)) {
