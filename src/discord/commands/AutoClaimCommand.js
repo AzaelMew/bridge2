9eb5ff68d0d796b169be74ad270fee859ed2a7d6
AzaelMew <dogydemon@gmail.com>
2023-01-13 13:34:37 +0200

    a

--- src/discord/commands/AutoClaimCommand.js
+++ src/discord/commands/AutoClaimCommand.js
@@ -77,19 +77,19 @@ async function getActivity(uuid, message) {
   let sblvl = data.data[0]?.sblevel
 
   if (sblvl >= 225) {
-    this.minecraft.bot.chat(`/g setrank ${data.data[0].username} Champion`)
+    this.minecraft.chat(`/g setrank ${data.data[0].username} Champion`)
     return
   }
   else if (sblvl >= 185) {
-    this.minecraft.bot.chat(`/g setrank ${data.data[0].username} Veteran`)
+    this.minecraft.chat(`/g setrank ${data.data[0].username} Veteran`)
     return
   }
   else if (sblvl >= 150) {
-    this.minecraft.bot.chat(`/g setrank ${data.data[0].username} Adventurer`)
+    this.minecraft.chat(`/g setrank ${data.data[0].username} Adventurer`)
     return
   }
   else {
-    this.minecraft.bot.chat(`/g setrank ${data.data[0].username} Initiate`)
+    this.minecraft.chat(`/g setrank ${data.data[0].username} Initiate`)
     return
   }
 }
