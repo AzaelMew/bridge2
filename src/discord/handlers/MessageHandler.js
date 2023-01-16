dffeb02d7c6e561927d3b4978d004186f0de9136
AzaelMew <dogydemon@gmail.com>
2023-01-16 15:22:28 +0200

    a

--- src/discord/handlers/MessageHandler.js
+++ src/discord/handlers/MessageHandler.js
@@ -55,7 +55,6 @@ class MessageHandler {
     return message
       .replace(/<[@|#|!|&]{1,2}(\d+){16,}>/g, '\n')
       .replace(/<:\w+:(\d+){16,}>/g, '\n')
-      .replace(/<.*:\d{18}>)/g,"\n")
       .split('\n')
       .map(part => {
         part = part.trim()
