c1941a2e9406f301e9002ab3bc8c650fdef3fe9c
AzaelMew <dogydemon@gmail.com>
2023-01-16 12:18:54 +0200

    a

---
+++ src/discord/commands/CataCommand.js
@@ -0,0 +1,84 @@
+const MinecraftCommand = require('../../contracts/MinecraftCommand')
+const axios = require("axios");
+function numberWithCommas(x) {
+  if (x > 999815672) {
+    x = x.toString().split(".")[0]
+    x = x.toString().slice(0, -6) + "815672";
+  }
+  else {
+    x = x.toString().split(".")[0]
+  }
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+async function getUUIDFromUsername(username) {
+  if (!(/^[a-zA-Z0-9_]{2,16}$/mg.test(username))) {
+    return "Error"
+  }
+  else {
+    const { data } = await axios.get('https://api.mojang.com/users/profiles/minecraft/' + username)
+    let uuid = data.id
+    let user = username
+    return data.id
+  }
+}
+async function getDungeonFromUsername(username) {
+  return await getDungeonFromUUID(await getUUIDFromUsername(username))
+}
+async function getDungeonFromUUID(name) {
+  try {
+    if (name == undefined) {
+      name = "a"
+    }
+    const { data } = await axios.get('http://161.35.22.13:187/v1/profiles/' + name + '?key=77ac89bad625453facaa36457eb3cf5c')
+    let secrets = data.data[0]?.dungeons.secrets_found
+    let lvl = data.data[0].dungeons?.catacombs?.skill?.levelWithProgress
+    let h = data.data[0].dungeons?.classes?.healer?.levelWithProgress
+    let m = data.data[0].dungeons?.classes?.mage?.levelWithProgress
+    let b = data.data[0].dungeons?.classes?.berserk?.levelWithProgress
+    let a = data.data[0].dungeons?.classes?.archer?.levelWithProgress
+    let t = data.data[0].dungeons?.classes?.tank?.levelWithProgress
+    let av = ((h + m + b + a + t) / 5)
+
+    let stats = "Cata: " + lvl.toFixed(2) + " ;Average: " + av.toFixed(2) + " ;Archer: " + a.toFixed(2) + " ;Berserk: " + b.toFixed(2) + " ;Healer: " + h.toFixed(2) + " ;Mage: " + m.toFixed(2) + " ;Tank: " + t.toFixed(2) + " ;Secrets: " + numberWithCommas(secrets)
+    return stats
+
+  }
+  catch (error) {
+    e = error.message
+    if (e.includes("status code 500")) {
+      return "is an Invalid Username"
+    }
+    if (e.includes("status code 404")) {
+      return "has no Skyblock Profiles"
+    }
+    else {
+      return error
+    }
+  }
+}
+class CatacombsCommand extends MinecraftCommand {
+  constructor(minecraft) {
+    super(minecraft)
+
+    this.name = 'cata'
+    this.description = "Says users dungeon stats"
+  }
+
+  async onCommand(username, message) {
+    let args = message.split(" ")
+    if (message.endsWith("!cata")) {
+      getDungeonFromUsername(username).then(stats => {
+        this.send(`/gc ${username}'s cata: ${stats.replaceAll(" ;", ", ").replaceAll("\n", "")}`)
+        this.minecraft.broadcastCommandEmbed({ username: `${username}'s cata`, message: `${stats.replaceAll(";", "\n")}` })
+      })
+    }
+    else {
+      getDungeonFromUsername(args[1]).then(stats => {
+        this.send(`/gc ${args[1]}'s cata: ${stats.replaceAll(" ;", ", ").replaceAll("\n", "")}`)
+        this.minecraft.broadcastCommandEmbed({ username: `${args[1]}'s cata`, message: `${stats.replaceAll(";", "\n")}` })
+      })
+    }
+  }
+}
+
+module.exports = CatacombsCommand
---
+++ src/discord/commands/SkillsCommand.js
@@ -0,0 +1,99 @@
+const DiscordCommand = require('../../contracts/DiscordCommand')
+const axios = require("axios");
+function numberWithCommas(x) {
+  if(x>999815672){
+    x = x.toString().split(".")[0]
+    x = x.toString().slice(0, -6) + "815672";
+  }
+  else{
+    x = x.toString().split(".")[0]
+  }
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+function round(value, precision) {
+  var multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
+}
+async function getUUIDFromUsername(username) {
+  if (!(/^[a-zA-Z0-9_]{2,16}$/mg.test(username))) {
+    return "Error"
+  }
+  else {
+    const { data } = await axios.get('https://api.mojang.com/users/profiles/minecraft/' + username)
+    let uuid = data.id
+    let user = username
+    return data.id
+  }
+}
+async function getSkillsFromUsername(username) {
+  return await getSkillsFromUUID(await getUUIDFromUsername(username))
+}
+async function getSkillsFromUUID(name) {
+  try {
+    if (name == undefined){
+      name = "a"
+    }
+    const { data } = await axios.get('http://161.35.22.13:187/v1/profiles/' + name + '?key=77ac89bad625453facaa36457eb3cf5c')
+    
+    let farming = data.data[0]?.skills?.farming.level
+    let mining = data.data[0]?.skills?.mining.level
+    let combat = data.data[0]?.skills?.combat.level
+    let foraging = data.data[0]?.skills?.foraging.level
+    let fishing = data.data[0]?.skills?.fishing.level
+    let enchant = data.data[0]?.skills?.enchanting.level
+    let alch = data.data[0]?.skills?.alchemy.level
+    let carp = data.data[0]?.skills?.carpentry.level
+    let rune = data.data[0]?.skills?.runecrafting.level
+    let soci = data.data[0]?.skills?.social.level
+    let taming = data.data[0]?.skills?.taming.level
+
+    let sa = round((farming + mining + combat + foraging + fishing + enchant + alch + taming + carp) / 9, 1)
+
+    let skills = `Skill Avg: ${sa}; Farm: ${farming}; Mine: ${mining}; Comb: ${combat}; Forage: ${foraging}; Fish: ${fishing}; Ench: ${enchant}; Alch: ${alch}; Carp: ${carp}; Rune: ${rune}; Soci: ${soci}; Taming: ${taming};`
+    return skills
+  }
+  catch (error) {
+    e = error.message
+    if(e.includes("status code 500")){
+      return "is an Invalid Username"
+    }
+    if(e.includes("status code 404")){
+      return "has no Skyblock Profiles"
+    }
+    else{
+      return error
+    }
+  }
+}
+
+
+
+class SkillsCommand extends DiscordCommand {
+  constructor(discord) {
+      super(discord)
+
+      this.name = 'stalk'
+      this.description = `Checks user's location`
+  }
+
+  async onCommand(username, message) {
+    let args = message.split(" ")
+    this.sendMinecraftMessage(`/gc ${user}'s skills: ${skills.replaceAll(";", ",").replaceAll("*","").replaceAll("\n➣ ","").replaceAll("\n","")}`)
+    message.channel.send({
+      embed: {
+        description: skills.replaceAll("; ", "\n").replaceAll(":",""),
+        color: '2A2A2A',
+        timestamp: new Date(),
+        footer: {
+          text: "BOT",
+        },
+        author: {
+          name: `${user}'s skills`,
+          icon_url: 'https://www.mc-heads.net/avatar/' + user,
+        },
+      },
+    })
+  }
+}
+
+module.exports = SkillsCommand
