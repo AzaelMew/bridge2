dc32a58c4fe41cef1693300c76fb2bacb9103082
AzaelMew <dogydemon@gmail.com>
2023-01-20 12:33:52 +0200

    a

--- src/discord/commands/SetrankCommand.js
+++ src/discord/commands/SetrankCommand.js
@@ -13,6 +13,18 @@ class SetrankCommand extends DiscordCommand {
     let user = args.shift()
     let rank = args.shift()
     console.log(user, rank)
+    if (rank = "ini"){
+      rank = "Initiate"
+    }
+    if (rank = "adv"){
+      rank = "Adventurer"
+    }
+    if (rank = "vet"){
+      rank = "Veteran"
+    }
+    if (rank = "champ"){
+      rank = "Champion"
+    }
     this.sendMinecraftMessage(`/g setrank ${user ? user : ''} ${rank ? rank: ''}`)
   }
 }
