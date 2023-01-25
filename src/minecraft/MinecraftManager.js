c96bdb5eea092369ce3007d4800f0a2e36a84050
AzaelMew <dogydemon@gmail.com>
2023-01-25 19:33:03 +0200

    a

--- src/minecraft/MinecraftManager.js
+++ src/minecraft/MinecraftManager.js
@@ -36,8 +36,8 @@ class MinecraftManager extends CommunicationBridge {
   }
 
   onBroadcast({ username, message, replyingTo }) {
-    this.app.log.broadcast(`${username}: `+ message, 'Minecraft')
-    if (this.bot.player !== undefined && message.length + username.length < 256) {
+    if (this.bot.player !== undefined && message.length + username.length < 254) {
+      this.app.log.broadcast(`${username}: `+ message, 'Minecraft')
       this.bot.chat(`/gc ${replyingTo ? `${username} replying to ${replyingTo}:` : `${username}:`} ${message}`)
     }
   }
