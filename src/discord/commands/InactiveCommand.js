3bc0f54965ad6e8a9518b420004dfa5e77a28860
AzaelMew <dogydemon@gmail.com>
2023-02-17 10:22:53 +0200

    a

--- src/discord/commands/InactiveCommand.js
+++ src/discord/commands/InactiveCommand.js
@@ -50,10 +50,10 @@ async function getGMemberFromUUID(uuid, message) {
           }
         }
         else if (i == data.guild.members.length + 1) {
-          for (s = 0; s < 220; s++) {
+          for (s = 0; s < 50; s++) {
             console.log(s)
             await new Promise(resolve => setTimeout(resolve, 50));
-            if (s == 220) {
+            if (s == 50) {
               return kickables
             }
           }
