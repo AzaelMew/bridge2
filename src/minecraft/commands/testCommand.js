9b98ef58ae3cc5daaf1fa15d1dec0e87b24582a5
AzaelMew <dogydemon@gmail.com>
2023-02-19 16:54:00 +0200

    a

--- src/minecraft/commands/testCommand.js
+++ src/minecraft/commands/testCommand.js
@@ -1,13 +1,9 @@
-// Step 1: Create a JavaScript object with the data you want to write to the file
-let myData = {
-    name: "John",
-    age: 30,
-    city: "New York"
-};
-
-// Step 2: Convert the JavaScript object to a JSON string using JSON.stringify() method
-let jsonString = JSON.stringify(myData);
-
+// Define new data to be added to the file
+const newData = {
+    name: 'Jane Doe',
+    age: 25,
+    email: 'jane.doe@example.com'
+  };
 // Step 3: Use Node.js fs module to write the JSON string to a file on your system
 const fs = require('fs');
 
