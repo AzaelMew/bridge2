98e40c3e04456b56a771cc0126d7a6d651fa41ce
AzaelMew <dogydemon@gmail.com>
2023-02-21 21:23:59 +0200

    a

--- src/discord/commands/KickCommand.js
+++ src/discord/commands/KickCommand.js
@@ -1,5 +1,15 @@
 const DiscordCommand = require('../../contracts/DiscordCommand')
-
+async function getUUIDFromUsername(username) {
+  if (!(/^[a-zA-Z0-9_]{2,16}$/mg.test(username))) {
+      return "Error"
+  }
+  else {
+      const { data } = await axios.get('https://api.mojang.com/users/profiles/minecraft/' + username)
+      let uuid = data.id
+      let user = username
+      return data.id
+  }
+}
 class KickCommand extends DiscordCommand {
   constructor(discord) {
     super(discord)
@@ -15,7 +25,17 @@ class KickCommand extends DiscordCommand {
     let reason = args.join(' ')
     if(user.toLowerCase()=="azael_nyaa") return
     else{
-      this.sendMinecraftMessage(`/g kick ${user ? user : ''} ${reason ? reason : ''}`)
+      getUUIDFromUsername(user).then(uuid => {
+        let blacklist = fs.readFileSync('/home/azael/bridge/blacklist.txt', 'utf-8');
+        let blacklistedIDs = blacklist.trim().split('\n');
+            if (!blacklist.includes(uuid)) {
+                this.send(`/oc ${args[2]} ${uuid} added to blacklist.`)
+                blacklist += uuid + "\n";
+  
+                fs.writeFileSync('/home/azael/bridge/blacklist.txt', blacklist, 'utf-8');
+            }
+        this.sendMinecraftMessage(`/g kick ${user ? user : ''} ${reason ? reason : ''}`)
+      })
     }
   }
 }
