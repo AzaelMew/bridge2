1eaf02e529845170d06675e30c54658a90ed05fe
AzaelMew <dogydemon@gmail.com>
2023-02-22 01:39:51 +0200

    a

---
+++ src/minecraft/commands/CropCommand.js
@@ -0,0 +1,45 @@
+const MinecraftCommand = require('../../contracts/MinecraftCommand')
+const axios = require("axios");
+function convertSecondsToMinutesAndSeconds(milliseconds) {
+    var minutes = Math.floor(milliseconds / 60000);
+    var seconds = ((milliseconds % 60000) / 1000).toFixed(0);
+    //let seconds = milliseconds;
+    //let minutes = Math.floor(seconds / 60);
+    //seconds = Math.floor(seconds % 60);
+    return (minutes < 10 ? "0" : "") + minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+}
+async function getJacobs(crop) {
+    const { data } = await axios.get("https://dawjaw.net/jacobs")
+    for (jEvent of data) {
+        let currentTime = Date.now();
+        let eventTime = jEvent['time'] * 1000;
+        if (currentTime < eventTime && jEvent['crops'].includes(crop)) {
+            let delta = eventTime - currentTime;
+            let timeUntilJacobEvent = convertSecondsToMinutesAndSeconds(delta);
+            let eventString = [];
+            jEvent['crops'].forEach((crop) => {
+                eventString.push(crop);
+            });
+            let contest = `The next contest with ${crop} is in ${timeUntilJacobEvent}`
+            return contest
+        }
+    }
+}
+class CropCommand extends MinecraftCommand {
+    constructor(minecraft) {
+        super(minecraft)
+
+        this.name = 'contest'
+
+        this.description = "Says users stats"
+    }
+
+    async onCommand(username, message) {
+        let args = message.split(" ")
+        getJacobs(args[1]).then(contest => {
+            this.send(`/gc ${contest}`)
+        })
+    }
+}
+
+module.exports = CropCommand
