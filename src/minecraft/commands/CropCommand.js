fb79329fcad56583c3d5663272a3f7e306692548
AzaelMew <dogydemon@gmail.com>
2023-02-22 14:46:12 +0200

    a

--- src/minecraft/commands/CropCommand.js
+++ src/minecraft/commands/CropCommand.js
@@ -19,6 +19,7 @@ function makeid(length) {
    return result;
   }
 async function capitalizeFirstLetter(string) {
+    string = string.toLowerCase()
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 async function getJacobs(crop) {
