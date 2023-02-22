e7119ac58bb7c863eb7a5fd5036b8c2203269639
AzaelMew <dogydemon@gmail.com>
2023-02-22 18:10:49 +0200

    a

--- src/minecraft/commands/CropCommand.js
+++ src/minecraft/commands/CropCommand.js
@@ -17,7 +17,7 @@ function makeid(length) {
   charactersLength));
    }
    return result;
-  }
+}
 async function capitalizeFirstLetter(string) {
     string = string.toLowerCase()
     return string.charAt(0).toUpperCase() + string.slice(1);
