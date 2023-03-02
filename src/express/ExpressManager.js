d389dcb51a877757a771169afadfaee68aba2e51
AzaelMew <dogydemon@gmail.com>
2023-03-02 15:49:47 +0200

    a

--- src/express/ExpressManager.js
+++ src/express/ExpressManager.js
@@ -272,7 +272,8 @@ class ExpressManager {
   patchnotes(request, response) {
     try {
       if (this.app.minecraft.bot?.player) {
-        this.app.minecraft.bot.chat(`/gc NEW PATCHNOTES! ${request.body.url}`)
+        console.log(request)
+        //this.app.minecraft.bot.chat(`/gc NEW PATCHNOTES! ${request.body.url}`)
 
         return response.status(200).json({
           success: true
