6f8b64b400ab647148d5ca41fc8019d8777207a8
AzaelMew <dogydemon@gmail.com>
2023-03-15 16:56:00 +0200

    a

--- src/minecraft/handlers/ChatHandler.js
+++ src/minecraft/handlers/ChatHandler.js
@@ -547,9 +547,10 @@ class StateHandler extends EventHandler {
     if (message.includes("i.imgur.com") || message.includes("cdn.discordapp.com/attachments")){
       let regex = /(?:^|\s)((?:(?:https?|ftp):\/\/|www\.)\S+(?:\b|$))/gm
       let url = message.match(regex)
+      playerMessage = playerMessage.replaceAll(url[0],"")
       this.minecraft.broadcastTextEmbed({
         username: username,
-        message: playerMessage.replace(url[0],""),
+        message: playerMessage,
         guildRank: guildRank,
         url: url[0],
       })
