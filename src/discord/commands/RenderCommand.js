e7f26ead2b3ff513cce0f923e37d46c7200b6872
AzaelMew <dogydemon@gmail.com>
2023-03-15 22:40:59 +0200

    a

--- src/discord/commands/RenderCommand.js
+++ src/discord/commands/RenderCommand.js
@@ -112,7 +112,6 @@ async function getPlayer(player, profile) {
     if (!mojangResponse) throw new Error('Player not found');
 
     const hypixelResponse = await axios.get(`https://api.hypixel.net/skyblock/profiles?uuid=${mojangResponse}&key=4fd2ea22-23ec-4543-9141-01288a80adfb`);
-    console.log(hypixelResponse)
     if (!hypixelResponse) throw new Error("Couldn't get a response from the API");
     if (hypixelResponse.profiles === null) throw new Error(`Couldn\'t find any Skyblock profile that belongs to ${player}`);
     let profileData = await getLastProfile(hypixelResponse);
