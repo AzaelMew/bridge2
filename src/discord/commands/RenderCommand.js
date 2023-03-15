c4a3e364a09dbb49304cf79634e54556dfb54fa2
AzaelMew <dogydemon@gmail.com>
2023-03-15 22:34:15 +0200

    a

--- src/discord/commands/RenderCommand.js
+++ src/discord/commands/RenderCommand.js
@@ -81,6 +81,7 @@ async function renderLore(itemName, lore) {
 }
 
 async function getLastProfile(data) {
+    console.log(data.data.profiles)
     const profiles = data.data.profiles;
     return profiles.sort((a, b) => b.selected - a.selected)[0];
 }
