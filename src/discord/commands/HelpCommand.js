c4073051150e14e127959a30edf8fe02d121c23e
AzaelMew <dogydemon@gmail.com>
2023-03-29 22:11:43 +0300

    a

--- src/discord/commands/HelpCommand.js
+++ src/discord/commands/HelpCommand.js
@@ -24,6 +24,7 @@ class HelpCommand extends DiscordCommand {
     })
 
     message.channel.send({
+      text: "https://media.discordapp.net/attachments/903390012584894484/1041616688749236385/TEMPEST_-_BRIDGE_2022.png?width=950&height=234",
       embeds: [{
         title: 'Help',
         description: 'Chat with people in-game and gain access to a variety of commands to use!\n\n',
@@ -53,9 +54,6 @@ class HelpCommand extends DiscordCommand {
             ].join('\n'),
           },
         ],
-        image: {
-          url: "https://media.discordapp.net/attachments/903390012584894484/1041616688749236385/TEMPEST_-_BRIDGE_2022.png?width=950&height=234"
-        },
         color: 0x2eebf4,
         footer: {
           text: 'Made by Azael'
