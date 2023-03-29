6abc1d6eab9aefd5a99091e7ef0de8bcc17828ba
AzaelMew <dogydemon@gmail.com>
2023-03-29 22:14:07 +0300

    a

--- src/discord/commands/HelpCommand.js
+++ src/discord/commands/HelpCommand.js
@@ -23,7 +23,8 @@ class HelpCommand extends DiscordCommand {
       minecraftCommands.push(`\`!${command.name}\`: ${command.description}`)
     })
 
-    message.channel.send("https://media.discordapp.net/attachments/903390012584894484/1041616688749236385/TEMPEST_-_BRIDGE_2022.png?width=950&height=234",{
+    message.channel.send({
+      content: "https://media.discordapp.net/attachments/903390012584894484/1041616688749236385/TEMPEST_-_BRIDGE_2022.png?width=950&height=234",
       embeds: [{
         title: 'Help',
         description: 'Chat with people in-game and gain access to a variety of commands to use!\n\n',
