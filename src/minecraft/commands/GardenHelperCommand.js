0f59e0a1edac5a9f5f79545365194d52e6ac97ef
AzaelMew <dogydemon@gmail.com>
2023-03-29 23:09:27 +0300

    a

--- src/minecraft/commands/GardenHelperCommand.js
+++ src/minecraft/commands/GardenHelperCommand.js
@@ -1,51 +1,55 @@
 const MinecraftCommand = require('../../contracts/MinecraftCommand')
 
 class GHCommand extends MinecraftCommand {
-  constructor(minecraft) {
-    super(minecraft)
+    constructor(minecraft) {
+        super(minecraft)
 
-    this.name = 'farmhelper'
-    this.aliases = ["fh"]
-    this.description = 'Gives needed welcome info'
-  }
+        this.name = 'farmhelper'
+        this.aliases = ["fh"]
+        this.description = 'Gives needed welcome info'
+    }
 
-  onCommand(username, message) {
-    let args = message.toLowerCase().split(" ")
-    if(args[1]== "cocoa"){
-        this.send(`/gc 150 speed w/o Sprint | 55° Angle Pitch`)
-    }
-    else if(args[1]== "cane"){
-        this.send(`/gc 327 Speed | 135° or 45° Angle Yaw`)
-    }
-    else if(args[1]== "potato"){
-        this.send(`/gc Normal: 360 w/ Sprint or 400 w/o Sprint | Angle doesn't matter`)
-    }
-    else if(args[1]== "carrot"){
-        this.send(`/gc Normal: 360 w/ Sprint or 400 w/o Sprint | Angle doesn't matter`)
-    }
-    else if(args[1]== "wart"){
-        this.send(`/gc Normal: 360 w/ Sprint or 400 w/o Sprint | Angle doesn't matter`)
-    }
-    else if(args[1]== "wheat"){
-        this.send(`/gc Normal: 360 w/ Sprint or 400 w/o Sprint | Angle doesn't matter`)
-    }
-    else if(args[1]== "pumpkin"){
-        this.send(`/gc Normal: 380 Speed | 145° or 35° Angle Yaw`)
+    onCommand(username, message) {
+        let args = message.toLowerCase().split(" ")
+        if (args[1] == "cocoa") {
+            this.send(`/gc 150 speed w/o Sprint | 55° Angle Pitch`)
+        }
+        else if (args[1] == "cane") {
+            this.send(`/gc 327 Speed | 135° or 45° Angle Yaw`)
+        }
+        else if (args[1] == "potato") {
+            this.send(`/gc Normal: 116 Speed (4 Lanes) or 93 Speed (5 Lanes) | 90° Yaw`)
+        }
+        else if (args[1] == "carrot") {
+            this.send(`/gc Normal: 116 Speed (4 Lanes) or 93 Speed (5 Lanes) | 90° Yaw`)
+        }
+        else if (args[1] == "wart") {
+            this.send(`/gc Normal: 116 Speed (4 Lanes) or 93 Speed (5 Lanes) | 90° Yaw`)
+        }
+        else if (args[1] == "wheat") {
+            this.send(`/gc Normal: 116 Speed (4 Lanes) or 93 Speed (5 Lanes) | 90° Yaw`)
+        }
+        else if (args[1] == "pumpkin") {
+            this.send(`/gc Normal: 155 Speed (Vertical Pumpkin) or 327 Speed (2 Lane) | 90° (Vert) and 145° or 35° (2 Lane) Angle Yaw`)
 
-    }
-    else if(args[1]== "melon"){
-        this.send(`/gc Normal: 380 Speed | 145° or 35° Angle Yaw`)
+        }
+        else if (args[1] == "melon") {
+            this.send(`/gc Normal: 155 Speed (Vertical Pumpkin) or 327 Speed (2 Lane) | 90° (Vert) and 145° or 35° (2 Lane) Angle Yaw`)
 
-    }
-    else if(args[1]== "cactus"){
-        this.send(`/gc 400 Speed | 90° Angle Yaw`)
+        }
+        else if (args[1] == "cactus") {
+            this.send(`/gc 400 Speed | 90° Angle Yaw`)
 
-    }
-    else{
-        setTimeout(() => {
-            this.send(`/gc Cocoa, Cane, Potato, Carrot, Wheat, Pumpkin, Melon, Cactus, Wart`)
-        }, 500);
-        this.send(`/gc Specify which crop you want to know about. The options are:`)
+        }
+        else if (args[1] == "mushroom") {
+            this.send(`/gc 233 Speed | 160° or 40° Angle Yaw`)
+
+        }
+        else {
+            setTimeout(() => {
+                this.send(`/gc Cocoa, Cane, Potato, Carrot, Wheat, Pumpkin, Melon, Cactus, Wart, Mushroom`)
+            }, 500);
+            this.send(`/gc Specify which crop you want to know about. The options are:`)
 
         }
     }
