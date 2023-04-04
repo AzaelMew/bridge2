73be523b60f0dfba658c2b1d7272a73b6cc387ad
AzaelMew <dogydemon@gmail.com>
2023-04-04 17:57:24 +0300

    a

--- src/discord/commands/AutoKickCommand.js
+++ src/discord/commands/AutoKickCommand.js
@@ -23,7 +23,7 @@ async function getGMemberFromUUID(uuid, message) {
     if (uuid == undefined) {
       uuid = "a"
     }
-    const { data } = await axios.get('https://api.hypixel.net/guild?key=${this.discord.app.config.discord.apikey}&player=' + uuid)
+    const { data } = await axios.get(`https://api.hypixel.net/guild?key=${this.discord.app.config.discord.apikey}&player=` + uuid)
     try {
       if (data.guild.name_lower != "tempestsky") {
         let ret = "This player is not in our guild."
