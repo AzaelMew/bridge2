d047f2464287e34f348c6af91b3dc4e76a4e5c0c
AzaelMew <dogydemon@gmail.com>
2023-06-24 18:34:20 +0300

    Cool Commands

---
+++ src/discord/commands/SleepCommand.js
@@ -0,0 +1,42 @@
+const DiscordCommand = require('../../contracts/DiscordCommand')
+const axios = require("axios");
+
+async function getNeko() {
+    const { data } = await axios.get('https://nekos.best/api/v2/sleep')
+    return data
+}
+
+class NekoCommand extends DiscordCommand {
+  constructor(discord) {
+    super(discord)
+
+    this.name = 'sleep'
+    this.description = 'sleep'
+    this.reqRole = "User"
+  }
+  onCommand(message) {
+    let author = message.author.id
+    let args = this.getArgs(message)
+    let user = args.shift()
+
+    getNeko().then(data => {
+        let neko = data.results[0].url
+        let anime = data.results[0].anime_name
+        message.channel.send({
+            embeds: [{
+                description: `Zzz.. <@${author}> sleeps...`,
+                image: {
+                    url: neko,
+                  },
+                color: 0x2A2A2A,
+                timestamp: new Date(),
+                footer: {
+                    text: `Anime: ${anime}`,
+                },
+            }],
+        })
+    })
+  }
+}
+
+module.exports = NekoCommand
