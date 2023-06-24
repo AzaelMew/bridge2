c2da8bbca7378bd4156811b2515365d7d5fc7ebb
AzaelMew <dogydemon@gmail.com>
2023-06-24 20:28:36 +0300

    test

--- src/discord/commands/InactiveCommand.js
+++ src/discord/commands/InactiveCommand.js
@@ -73,7 +73,9 @@ async function getActivity(uuid, message) {
     const { data } = await axios.get(`https://api.hypixel.net/player?uuid=${uuid}&key=${process.env.APIKEY}`)
     let lastLogin = data.player.lastLogin
     if (new Date().getTime() - lastLogin > 1209600000) {
-      kickables.push(`${data.player.displayname} - ${lastLogin}`)
+      lastSeen = new Date(lastLogout)
+      lastSeen = lastSeen.toUTCString()
+      kickables.push(`${data.player.displayname} - ${lastSeen}\n`)
       console.log(`${kickables}`)
       return kickables
     }
